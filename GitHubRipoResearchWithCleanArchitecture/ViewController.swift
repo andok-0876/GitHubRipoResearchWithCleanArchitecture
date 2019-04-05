73775578c8e9b8941abb51eb606326c5f6360f67
andok-0876 <kazutaka.ando@sun-asterisk.com>
2019-04-05 08:14:36 +0700

    Initial Commit

---
+++ GitHubRipoResearchWithCleanArchitecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubRipoResearchWithCleanArchitecture
+//
+//  Created by 安藤 on 2019/04/05.
+//  Copyright © 2019 kazando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
